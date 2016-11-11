9dfd5a30d37a6156f4b894306fd30ec71eebb830
jackjhall22 <jackjhall22@me.com>
2016-11-10 19:28:23 -0600

    refactor to functional instantiation from pseudoclassical add handlers object to handle events make some buttons and connect display button to the todoList.displayTodos method

--- js/app.js
+++ js/app.js
@@ -1,63 +1,78 @@
-var Todos = function(){
-	this.todoList = [];
-}
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+  	if (this.todos.length ===0){
+  		console.log('Your todo lest is empty!');
+  	} else {
+  		console.log('My Todos:');
+  		for (var i = 0; i < this.todos.length; i++) {
+  			if (this.todos[i].completed === true) {
+  				console.log('(x)', this.todos[i].todoText);
+  			} else {
+  				console.log('( )', this.todos[i].todoText);
+  			}
+  		}
+  	}
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
 
-Todos.prototype.displayTodos = function(){
-	this.todoList.forEach(function(todo){
-		console.log(todo);
-	})
-}
+    // Get number of completed todos.
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    
+    // Case 1: If everything’s true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var ii = 0; ii < totalTodos; ii++) {
+        this.todos[ii].completed = false;
+      }
+    // Case 2: Otherwise, make everything true.
+    } else {
+      for (var iii = 0; iii < totalTodos; iii++) {
+        this.todos[iii].completed = true;
+      }
+    }
+  }
+};
 
-Todos.prototype.add = function(todoText){
-	this.todoList.push({
-		todoText: todoText,
-		completed: false
-	})
-	this.displayTodos();
-}
+var handlers = {
+	displayTodos: function(){
+		todoList.displayTodos();
+	}
+};
 
-Todos.prototype.changeTodo = function(position, todoText){
-	this.todoList.forEach(function(todo, index){
-		if (position === index) {
-			this.todoList[index].todoText = todoText;
-		}
-	}, this);
-	this.displayTodos();
-}
+// var handlers = {
+//   addTodo: function() {
+//     var addTodoTextInput = document.getElementById('addTodoTextInput');
+//     todoList.addTodo(addTodoTextInput.value);
+//     addTodoTextInput.value = '';
+//     view.displayTodos();
+//   },
 
-Todos.prototype.delete = function(position){
-	this.todoList.splice(position, 1);
-	this.displayTodos();
-}
 
-Todos.prototype.toggleCompleted = function(position){
-	var todo = this.todoList[position];
-	todo.completed = !todo.completed;
-	this.displayTodos();
-}
 
-Todos.prototype.toggleAll = function(){
-	var completedTodo = 0;
-	var totalTodos = this.todoList.length
-	// find number of completed
-	this.todoList.forEach(function(todo){
-		if (todo.completed === true){
-			completedTodo++;
-		}
-	})
-	this.todoList.forEach(function(todo){
-	//if everything is true then change everything to false
-		if (completedTodo === totalTodos){
-			todo.completed = false;
-	//else make everything true;
-		}else{
-			todo.completed = true;
-		}
-	})
-	this.displayTodos();
-}
 
-var handlers = new Todos();
+
 
 
 
