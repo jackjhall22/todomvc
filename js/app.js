cf1d6ee21fd8d84f1f82866722092d32133a84f3
jackjhall22 <jackjhall22@me.com>
2016-11-15 20:40:16 -0600

    move out of the console and append to the body of the HTML page

--- js/app.js
+++ js/app.js
@@ -1,37 +1,39 @@
 var todoList = {
   todos: [],
   
-displayTodos: function() {
-    if (this.todos.length ===0){
-      console.log('Your todo lest is empty!');
-    } else {
-      console.log('My Todos:');
-      for (var i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed === true) {
-          console.log('(x)', this.todos[i].todoText);
-        } else {
-          console.log('( )', this.todos[i].todoText);
-        }
-      }
-    }
-  },
+// displayTodos: function() {
+//     if (this.todos.length ===0){
+//       console.log('Your todo lest is empty!');
+//     } else {
+//       console.log('My Todos:');
+//       for (var i = 0; i < this.todos.length; i++) {
+//         if (this.todos[i].completed === true) {
+//           console.log('(x)', this.todos[i].todoText);
+//         } else {
+//           console.log('( )', this.todos[i].todoText);
+//         }
+//       }
+//     }
+//   },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
+    view.displayTodos();
   },
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
+    view.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    view.displayTodos();
   },
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
+    view.displayTodos();
   },
   toggleAll: function() {
     var totalTodos = this.todos.length;
@@ -55,6 +57,7 @@ displayTodos: function() {
         this.todos[i].completed = true;
       }
     }
+    view.displayTodos();
   }
 };
 
